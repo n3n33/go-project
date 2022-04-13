b34c984df051c6fa7afaf848b3a2b9c4f396b8b5
n3n33 <whi0331@hanmail.net>
2022-04-13 17:04:04 +0900

    hit url & checking

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var errRequestFailed = errors.New("Request Failed")
 
 // hit URL
 func main() {
@@ -9,6 +15,17 @@ func main() {
 		"www.google.com",
 	}
 	for _, url := range urls {
-		fmt.Println(url)
+		//fmt.Println(url)
+		hitURL(url)
 	}
+}
+
+//웹사이트에 접속
+func hitURL(url string) error {
+	fmt.Println("Checking: ", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
